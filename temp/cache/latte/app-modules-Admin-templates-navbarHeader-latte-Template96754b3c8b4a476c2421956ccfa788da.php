ea439c358f8895248a1388f3d05c72b5551414eb
jackobboss <jackob@jackob.cz>
2016-02-12 05:39:11 +0100

    sa
    a

---
+++ temp/cache/latte/app-modules-Admin-templates-navbarHeader-latte-Template96754b3c8b4a476c2421956ccfa788da.php
@@ -0,0 +1,31 @@
+<?php
+// source: C:\wamp\www\titan\app\modules\Admin/templates/navbarHeader.latte
+
+class Template96754b3c8b4a476c2421956ccfa788da extends Latte\Template {
+function render() {
+foreach ($this->params as $__k => $__v) $$__k = $__v; unset($__k, $__v);
+// prolog Latte\Macros\CoreMacros
+list($_b, $_g, $_l) = $template->initialize('538c5ad2a5', 'html')
+;
+// prolog Nette\Bridges\ApplicationLatte\UIMacros
+
+// snippets support
+if (empty($_l->extends) && !empty($_control->snippetMode)) {
+	return Nette\Bridges\ApplicationLatte\UIMacros::renderSnippets($_control, $_b, get_defined_vars());
+}
+
+//
+// main template
+//
+?>
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html"><i class="fa fa-gears"></i> Titan Storage CRM</a>
+            </div>
+            <!-- /.navbar-header --><?php
+}}
